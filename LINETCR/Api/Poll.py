d85dabf0233528fad6046e00e5ad441f192d4456
RadityoKalani <33354192+1DaffaN3kalani@users.noreply.github.com>
2018-01-26 14:38:52 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,7 +18,7 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/7.14.0 iPad5,1 10.2.0"
+  UA = "Line/6.0.0 iPad4,1 9.0.2"
   LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
 
   rev = 0
